837eb41c8e2544cb351315c2f80f3d1cbf86714b
ayushipooniaa <161065328+ayushipooniaa@users.noreply.github.com>
2025-06-27 11:26:44 +0530

    Updated all files with latest changes

--- client/src/conext/AppContext.jsx
+++ client/src/conext/AppContext.jsx
@@ -5,7 +5,6 @@ import { toast } from 'react-hot-toast';
 import { useNavigate } from "react-router-dom";
 import { assets } from "../assets/assets";
 
-
 axios.defaults.baseURL = import.meta.env.VITE_BACKEND_URL;
 
 const AppContext = createContext();
